8132ae5d18403f17500b25cc6febd33eb0fd65d0
jbolanos-2024525 <jbolanos-2024525@kinal.edu.gt>
2026-03-05 19:16:09 -0600

    Feat(RBK-006): Se agregaron las configuraciones de node para la funcionalidad del proyecto.

---
+++ auth-node/config/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { corsOptions } from './cors.configuration.js';
+import { helmetOptions } from './helmet.configuration.js';
+import { dbConnection } from './db.configuration.js';
+import { requestLimit } from './rate.limit.configuration.js';
+import eventoRoutes from '../src/evento/evento.routes.js';
+import avisosRoutes from '../src/avisos/avisos.routes.js';
+import massScheduleRoutes from '../src/MassSchedule/massSchedule.routes.js';
+import categoriaRoutes from '../src/categoria/categoria.routes.js';
+
+const BASE_PATH = '/SistemaParroquial/v1';
+
+const routes = (app) => {
+    app.use(`${BASE_PATH}/eventos`, eventoRoutes);
+    app.use(`${BASE_PATH}/avisos`, avisosRoutes);
+    app.use(`${BASE_PATH}/misa`, massScheduleRoutes);
+    app.use(`${BASE_PATH}/categorias`, categoriaRoutes);
+
+    app.get(`${BASE_PATH}/health`, (req, res) => {
+        res.status(200).json({
+            status: 'Healthy',
+            timeStamp: new Date().toISOString(),
+            service: 'Sistema Parroquial Server'
+        });
+    });
+
+    app.use((req, res) => {
+        res.status(404).json({
+            success: false,
+            message: 'EndPoint no encontrado'
+        });
+    });
+};
+
+const middlewares = (app) => {
+    app.use(express.json({ limit: '10mb' }));
+    app.use(express.urlencoded({ extended: false, limit: '10mb' }));
+    app.use(cors(corsOptions));
+    app.use(helmet(helmetOptions));
+    app.use(morgan('dev'));
+    app.use(requestLimit);
+};
+
+export const initServer = async () => {
+    const app = express();
+    const PORT = process.env.PORT || 3000;
+    app.set('trust proxy', 1);
+
+    try {
+        middlewares(app);
+        await dbConnection();
+        routes(app);
+
+        app.listen(PORT, () => {
+            console.log(`Sistema Parroquial corriendo en puerto ${PORT}`);
+            console.log(`Health check endpoint: http://localhost:${PORT}${BASE_PATH}/health`);
+        });
+    } catch (err) {
+        console.error(`Sistema Parroquial - Error al iniciar el servidor: ${err.message}`);
+        process.exit(1);
+    }
+};
---
+++ auth-node/config/cors.configuration.js
@@ -0,0 +1,6 @@
+export const corsOptions = {
+    origin: true,
+    Credential: true,
+    methods: ['GET', 'POST', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
