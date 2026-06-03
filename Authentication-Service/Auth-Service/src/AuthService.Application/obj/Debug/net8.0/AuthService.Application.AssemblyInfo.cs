12162f412371a15848797fd0231f1c73911880c4
jmendez-2024055 <jmendez-2024055@kinal.edu.gt>
2026-06-02 20:34:19 -0600

    Feat(RBK-012): Se añadio la estructura para iniciar trabajos de fronted

--- Authentication-Service/Auth-Service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
+++ Authentication-Service/Auth-Service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ee5d0d4891fbbedbfefc915d27d4620fcfa7679")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b151c4ade1c27ee9234ec55fe1af44f48b8fa412")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
